9b1f0f16d67d35157d3c9a501364023657fecb7c
Kefu Chai <kchai@redhat.com>
2016-08-29 19:52:19 +0800

    tools/ceph-objectstore-tool: add "update-mon-db" command
    Fixes: http://tracker.ceph.com/issues/17179
    Signed-off-by: Kefu Chai <kchai@redhat.com>

---
+++ src/tools/rebuild_mondb.h
@@ -0,0 +1,8 @@
+#include <string>
+
+class ObjectStore;
+class OSDSuperblock;
+
+int update_mon_db(ObjectStore& fs, OSDSuperblock& sb,
+                  const std::string& keyring_path,
+                  const std::string& store_path);
